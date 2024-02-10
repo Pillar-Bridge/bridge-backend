a4f73b05f7848ba6ec019d9bf5f058e86bce94fe
MiraeKim <hufs202000512@gmail.com>
2024-02-10 13:55:57 +0900

    fix: token error response

--- src/main/java/com/pillar/bridge/util/jwt/JwtExceptionFilter.java
+++ src/main/java/com/pillar/bridge/util/jwt/JwtExceptionFilter.java
@@ -7,12 +7,14 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+@Component
 public class JwtExceptionFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
