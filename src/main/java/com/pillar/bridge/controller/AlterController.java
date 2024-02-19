e1061a38ec87e5afff2e62111d9090893ff38dae
MiraeKim <hufs202000512@gmail.com>
2024-02-20 01:17:50 +0900

    feat: error handling

--- src/main/java/com/pillar/bridge/controller/AlterController.java
+++ src/main/java/com/pillar/bridge/controller/AlterController.java
@@ -2,26 +2,34 @@ package com.pillar.bridge.controller;
 
 import com.pillar.bridge.apiUtils.ResponseDto;
 import com.pillar.bridge.apiUtils.ResponseUtil;
+import com.pillar.bridge.apiUtils.codeStatus.ErrorResponse;
 import com.pillar.bridge.apiUtils.codeStatus.SuccessResponse;
 import com.pillar.bridge.dto.alter.AlterResponse;
 import com.pillar.bridge.dto.alter.AlterRequest;
+import com.pillar.bridge.repository.DeviceRepository;
 import com.pillar.bridge.service.AlterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AlterController {
     private final AlterService alterService;
+    private final DeviceRepository deviceRepository;
 
     @Autowired
-    public AlterController(AlterService alterService) {
+    public AlterController(AlterService alterService, DeviceRepository deviceRepository) {
         this.alterService = alterService;
+        this.deviceRepository = deviceRepository;
     }
 
     @PostMapping("/provide-modification-options")
-    public ResponseDto<AlterResponse> getOptions(@RequestBody AlterRequest request) {
+    public ResponseDto<AlterResponse> getOptions(@RequestBody AlterRequest request, @RequestHeader("UUID") String uuid) {
+        if (deviceRepository.findByUuid(uuid).isEmpty()) {
+            return ResponseUtil.FAILED(ErrorResponse.BAD_REQUEST, "UUID가 유효하지 않습니다", null);
+        }
         return ResponseUtil.SUCCESS(SuccessResponse.OK, "요청에 성공했습니다", alterService.getOptions(request));
     }
 }
--- src/main/java/com/pillar/bridge/controller/ReplyController.java
+++ src/main/java/com/pillar/bridge/controller/ReplyController.java
@@ -2,14 +2,18 @@ package com.pillar.bridge.controller;
 
 import com.pillar.bridge.apiUtils.ResponseDto;
 import com.pillar.bridge.apiUtils.ResponseUtil;
+import com.pillar.bridge.apiUtils.codeStatus.ErrorResponse;
 import com.pillar.bridge.apiUtils.codeStatus.SuccessResponse;
 import com.pillar.bridge.entitiy.Messages;
+import com.pillar.bridge.repository.DeviceRepository;
+import com.pillar.bridge.repository.DialogueRepository;
 import com.pillar.bridge.repository.MessageRepository;
 import com.pillar.bridge.service.ReplyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,11 +24,22 @@ public class ReplyController {
 
     @Autowired
     private ReplyService replyService;
+    @Autowired
+    private DeviceRepository deviceRepository;
+
+    @Autowired
+    private DialogueRepository dialogueRepository;
 
     @GetMapping("/recommend-replies")
-    public ResponseDto<Map<String, Object>> getLatestMessageResponse(@RequestParam("dialogueId") String dialogueId) {
+    public ResponseDto<Map<String, Object>> getLatestMessageResponse(@RequestParam("dialogueId") String dialogueId, @RequestHeader("UUID") String uuid) {
+        if (deviceRepository.findByUuid(uuid).isEmpty()) {
+            return ResponseUtil.FAILED(ErrorResponse.BAD_REQUEST, "UUID가 유효하지 않습니다", null);
+        }
+        //dialogueID 유효성 검증
+        if (dialogueRepository.findById(dialogueId).isEmpty()) {
+            return ResponseUtil.FAILED(ErrorResponse.BAD_REQUEST, "dialogueId가 유효하지 않습니다", null);
+        }
         Map<String, Object> response = replyService.getLatestMessageResponse(dialogueId);
-
         return ResponseUtil.SUCCESS(SuccessResponse.OK, "성공적으로 반영되었습니다", response);
     }
 }
